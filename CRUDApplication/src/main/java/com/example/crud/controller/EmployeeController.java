f329388e993baaa663be25607e5bf216775cb2ad
kuberreddy123 <kubereddyp@gmail.com>
2024-12-22 11:56:52 +0530

    changed Delete method comment..

--- CRUDApplication/src/main/java/com/example/crud/controller/EmployeeController.java
+++ CRUDApplication/src/main/java/com/example/crud/controller/EmployeeController.java
@@ -59,7 +59,7 @@ public class EmployeeController {
         return new ResponseEntity<Employee>(employeeService.updateEmployee(employee), HttpStatus.OK);
     }
 
-    //This method is to delete employee based on id, if the employee exists with particulat id
+    //This method is to delete employee based on id, if the employee exists with particular id
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteEmployee(@PathVariable int id) throws EmployeeNotFoundExcepiton {
 
